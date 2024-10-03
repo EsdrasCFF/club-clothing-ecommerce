60b8a23d82dc77c4c14005907b97c2d3c456f304
Esdras Castro <ecff.dev@gmail.com>
2024-10-03 00:34:13 -0300

    feat: add Routes and Route of the react-router-dom

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,16 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import Home from './pages/home'
 import { Header } from './components/header'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <div className="flex h-full flex-col">
+      <div className="flex h-screen flex-col">
         <Header />
-        <Home />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
       </div>
     </BrowserRouter>
   </StrictMode>
