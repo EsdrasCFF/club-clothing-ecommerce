c62ffbb130476e62c7a86656f66eb212533b5316
Esdras Castro <ecff.dev@gmail.com>
2024-10-04 09:39:39 -0300

    feat: add react-hook-form and zodValidator

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,13 +1,39 @@
+import { zodResolver } from '@hookform/resolvers/zod'
 import {} from 'lucide-react'
+import { useForm } from 'react-hook-form'
 import { CiLogin } from 'react-icons/ci'
 import { FaGoogle } from 'react-icons/fa'
+import { z } from 'zod'
 
 import { CustomButton } from '@/components/custom-button'
-import { CustomInput } from '@/components/custom-input'
+import CustomInput from '@/components/custom-input'
 
 import { Separator } from '../../components/ui/separator'
 
+const accountLoginSchema = z.object({
+  email: z.string({ required_error: 'Email is required' }).email({ message: 'Invalid e-mail' }),
+  password: z
+    .string({ required_error: 'Password is required' })
+    .min(6, { message: 'Shold be greater or equal 6 characters' }),
+})
+
+type AccountLoginData = z.infer<typeof accountLoginSchema>
+
 export function LoginPage() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<AccountLoginData>({
+    resolver: zodResolver(accountLoginSchema),
+  })
+
+  console.log(errors)
+
+  function onSubmit(data: AccountLoginData) {
+    console.log(data)
+  }
+
   return (
     <main className="flex h-full w-full items-center justify-center px-5">
       <div className="flex w-full max-w-[28rem] flex-col items-center justify-center gap-5">
@@ -15,9 +41,22 @@ export function LoginPage() {
         <CustomButton icon={FaGoogle} title="Entrar com Google" className="w-full" />
         <p className="font-medium">ou entre com seu e-mail</p>
         <Separator className="bg-gray4" />
-        <CustomInput label="E-mail" placeholder="Digite seu e-mail" />
-        <CustomInput label="Senha" placeholder="Digite sua senha" />
-        <CustomButton icon={CiLogin} title="Entrar" />
+        <CustomInput
+          label="E-mail"
+          placeholder="Digite seu e-mail"
+          {...register('email')}
+          hasError={!!errors.email}
+          errorMessage={errors.email?.message}
+        />
+        <CustomInput
+          type="password"
+          label="Senha"
+          placeholder="Digite sua senha"
+          {...register('password')}
+          hasError={!!errors.password}
+          errorMessage={errors.password?.message}
+        />
+        <CustomButton icon={CiLogin} title="Entrar" onClick={handleSubmit(onSubmit)} />
       </div>
     </main>
   )
