915ba5624662ecc6ceabbd340d7b146a6901ab16
Esdras Castro <ecff.dev@gmail.com>
2024-10-07 22:28:01 -0300

    feat: add toastfy and provider

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import './index.css'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Toaster } from 'sonner'
 
 import { Header } from './components/header'
 import Home from './pages/home'
@@ -12,6 +13,7 @@ import { SignUpPage } from './pages/sign-up'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
+      <Toaster richColors />
       <div className="flex h-screen flex-col items-center">
         <Header />
         <div className="flex h-full w-full max-w-screen-xl flex-col">
