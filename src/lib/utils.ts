01c0379b10ecbf98eabd20176ae22aab4075b9da
Esdras Castro <ecff.dev@gmail.com>
2024-10-10 11:57:37 -0300

    feat: add currencyFormar helper function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,10 @@
-import { clsx, type ClassValue } from 'clsx'
+import { type ClassValue,clsx } from 'clsx'
 import { twMerge } from 'tailwind-merge'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function currencyFormat(value: number) {
+  return Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(value)
 }
