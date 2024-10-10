673d8499be1fe84e10a97aff398d0af270d283ea
Esdras Castro <ecff.dev@gmail.com>
2024-10-10 12:01:39 -0300

    feat: add ExporePage

--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,7 @@ import { Toaster } from 'sonner'
 import { Header } from './components/header'
 import { CategoryContextProvider } from './contexts/category-context'
 import { UserContextProvider } from './contexts/user-context'
+import ExplorePage from './pages/explore'
 import Home from './pages/home'
 import { SignInPage } from './pages/sign-in'
 import { SignUpPage } from './pages/sign-up'
@@ -20,11 +21,12 @@ createRoot(document.getElementById('root')!).render(
           <Toaster richColors />
           <div className="flex h-screen flex-col items-center">
             <Header />
-            <div className="flex h-full w-full max-w-screen-xl flex-col">
+            <div className="flex w-full max-w-screen-xl flex-1 flex-col">
               <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/login" element={<SignInPage />} />
                 <Route path="/sign-up" element={<SignUpPage />} />
+                <Route path="/explore" element={<ExplorePage />} />
               </Routes>
             </div>
           </div>
---
+++ src/pages/explore/index.tsx
@@ -0,0 +1,9 @@
+import { OverviewCategories } from '@/components/overview-categories'
+
+export default function ExplorePage() {
+  return (
+    <main className="flex h-full w-full px-5">
+      <OverviewCategories />
+    </main>
+  )
+}
