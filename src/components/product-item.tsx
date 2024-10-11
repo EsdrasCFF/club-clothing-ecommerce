1a18277b2902c5241748b7a4d5c7e41bb315d8a1
Esdras Castro <ecff.dev@gmail.com>
2024-10-11 12:44:56 -0300

    feat: add function addProductToCart and react

--- src/components/product-item.tsx
+++ src/components/product-item.tsx
@@ -1,27 +1,41 @@
 import { ShoppingBag } from 'lucide-react'
+import { useContext } from 'react'
+import { toast } from 'sonner'
 
+import { CartContext } from '@/contexts/cart-context'
 import { currencyFormat } from '@/lib/utils'
 
+import { Product } from './categories-area'
 import { CustomButton } from './custom-button'
 
 interface CategoryProductPros {
-  imageUrl: string
-  name: string
-  price: number
+  product: Product
 }
 
-export function ProductItem({ imageUrl, name, price }: CategoryProductPros) {
+export function ProductItem({ product }: CategoryProductPros) {
+  const { addProductToCart } = useContext(CartContext)
+
+  function handleAddProductToCart(product: Product) {
+    addProductToCart(product)
+    toast.success(`${product.name} foi adicionado ao carrinho!`, { position: 'top-right' })
+  }
+
   return (
     <div className="h-full w-full">
       <div className="group relative h-full max-h-96 w-full max-w-[18.75rem] overflow-hidden rounded-md shadow-lg shadow-gray2">
-        <img src={imageUrl} alt={`image do producto ${name}`} className="h-full w-full object-cover" />
+        <img src={product.imageUrl} alt={`image do producto ${product.name}`} className="h-full w-full object-cover" />
         <div className="absolute bottom-0 flex h-full w-full items-end bg-black3/25 px-5 pb-5 opacity-0 transition-opacity duration-300 ease-in-out group-hover:opacity-100">
-          <CustomButton icon={ShoppingBag} title="Adicionar ao carrinho" className="text-sm" />
+          <CustomButton
+            icon={ShoppingBag}
+            title="Adicionar ao carrinho"
+            className="text-sm"
+            onClick={() => handleAddProductToCart(product)}
+          />
         </div>
       </div>
       <div className="mt-2 flex items-center justify-between text-sm font-medium leading-none">
-        <span className="capitalize">{name}</span>
-        <span>{currencyFormat(price)}</span>
+        <span className="capitalize">{product.name}</span>
+        <span>{currencyFormat(product.price)}</span>
       </div>
     </div>
   )
