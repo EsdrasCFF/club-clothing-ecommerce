1951f2fa0d7f652d55a262b697e9584abcc98853
Esdras Castro <ecff.dev@gmail.com>
2024-10-11 12:28:17 -0300

    style: add className as prop to the CustomButton component

--- src/components/custom-button.tsx
+++ src/components/custom-button.tsx
@@ -2,17 +2,20 @@ import { LucideIcon } from 'lucide-react'
 import { ComponentProps } from 'react'
 import { IconType } from 'react-icons'
 
+import { cn } from '@/lib/utils'
+
 import { Button } from './ui/button'
 
 interface CustomButtonProps extends ComponentProps<'button'> {
   icon: LucideIcon | IconType
   title: string
+  className?: string
 }
 
-export function CustomButton({ icon: Icon, title, ...rest }: CustomButtonProps) {
+export function CustomButton({ icon: Icon, title, className, ...rest }: CustomButtonProps) {
   return (
     <Button {...rest} className="w-full">
-      <Icon size={25} /> <span className="ml-2 font-semibold text-white2">{title}</span>
+      <Icon size={25} /> <span className={cn('ml-2 font-semibold text-white2', className)}>{title}</span>
     </Button>
   )
 }
