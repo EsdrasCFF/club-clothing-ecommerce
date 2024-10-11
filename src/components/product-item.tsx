4f8f507ef38c095d2b9b075a1b88f16931fb9e7a
Esdras Castro <ecff.dev@gmail.com>
2024-10-11 10:27:27 -0300

    style: add hover and transition  in product-item component

--- src/components/product-item.tsx
+++ src/components/product-item.tsx
@@ -1,4 +1,8 @@
+import { ShoppingBag } from 'lucide-react'
+
 import { currencyFormat } from '@/lib/utils'
+
+import { CustomButton } from './custom-button'
 
 interface CategoryProductPros {
   imageUrl: string
@@ -9,8 +13,11 @@ interface CategoryProductPros {
 export function ProductItem({ imageUrl, name, price }: CategoryProductPros) {
   return (
     <div className="h-full w-full">
-      <div className="h-full max-h-96 w-full max-w-[18.75rem] overflow-hidden rounded-md shadow-lg shadow-gray2">
+      <div className="group relative h-full max-h-96 w-full max-w-[18.75rem] overflow-hidden rounded-md shadow-lg shadow-gray2">
         <img src={imageUrl} alt={`image do producto ${name}`} className="h-full w-full object-cover" />
+        <div className="absolute bottom-0 flex h-full w-full items-end bg-black3/25 px-5 pb-5 opacity-0 transition-opacity duration-300 ease-in-out group-hover:opacity-100">
+          <CustomButton icon={ShoppingBag} title="Adicionar ao carrinho" className="text-sm" />
+        </div>
       </div>
       <div className="mt-2 flex items-center justify-between text-sm font-medium leading-none">
         <span className="capitalize">{name}</span>
