bcdfb3c7ab342d96d8913e6abde320b013bfc45f
Esdras Castro <ecff.dev@gmail.com>
2024-10-15 00:32:46 -0300

    fix: remove category reducer

--- src/store/reducers/category/category.actions-types.ts
+++
@@ -1,5 +0,0 @@
-export const CategoryActionsTypes = {
-  FETCH_CATEGORIES_START: 'categories/fetchStart' as const,
-  FETCH_CATEGORIES_SUCCESS: 'categories/fetchSuccess' as const,
-  FETCH_CATEGORIES_FAILURE: 'categories/fetchFeilure' as const,
-}
